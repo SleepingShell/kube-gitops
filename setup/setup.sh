a2a9606cbcc30393f918e50bd50cd3f20a655635
SleepingShell <example@example.com>
2021-01-13 16:07:13 -0500

    Install Cilium + Switch to Flux v2

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,12 +6,40 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 kubectl create namespace flux
 
-echo "Installing flux"
+
 
 helm repo add fluxcd https://charts.fluxcd.io
+helm repo add cilium https://helm.cilium.io/
 helm repo update
-helm upgrade --install flux --values "$REPO_ROOT"/flux/flux-values.yaml --namespace flux fluxcd/flux
-helm upgrade --install helm-operator --values "$REPO_ROOT"/flux/flux-helm-operator-values.yaml --namespace flux fluxcd/helm-operator
+
+echo "Installing Cilium"
+helm -n kube-system -f "$REPO_ROOT"/kube-system/cilium/cilium-values.yaml upgrade --install cilium cilium/cilium
+
+sleep 10
+
+echo "installing fluxv2"
+flux check --pre > /dev/null
+FLUX_PRE=$?
+if [ $FLUX_PRE != 0 ]; then 
+  echo -e "flux prereqs not met:\n"
+  flux check --pre
+  exit 1
+fi
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN is not set! Check $REPO_ROOT/setup/.env"
+  exit 1
+fi
+flux bootstrap github \
+  --owner=sleepingshell \
+  --repository=kube-gitops \
+  --branch master \
+  --personal
+
+FLUX_INSTALLED=$?
+if [ $FLUX_INSTALLED != 0 ]; then 
+  echo -e "flux did not install correctly, aborting!"
+  exit 1
+fi
 
 FLUX_READY=1
   while [ $FLUX_READY != 0 ]; do
@@ -22,5 +50,5 @@ FLUX_READY=1
   done
 echo "Calling add-repo..."
 
-FLUX_KEY=$(kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2)
-"$REPO_ROOT"/setup/add-repo-key.sh "$FLUX_KEY"
+#FLUX_KEY=$(kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2)
+#"$REPO_ROOT"/setup/add-repo-key.sh "$FLUX_KEY"
